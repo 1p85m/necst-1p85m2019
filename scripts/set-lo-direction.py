1b2c4407ade47d795560d6af49dac08666c40e15
近藤滉 <kondo@hiroshinoMacBook-Air.local>
2019-12-02 21:10:11 +0900

    add lo

--- scripts/set-lo-direction.py
+++ scripts/set-lo-direction.py
@@ -13,9 +13,17 @@ rospy.init_node(name)
 
 sis = controller_1p85m2019.sglo()
 
-f1 = 
+f1 = 225
+f2 = 235.6
+f3 = 4.0
+f4 = 6.5
 
 sis.set_1st_lo(float(f1),"lhcp")
 sis.set_1st_lo(float(f2),"rhcp")
 sis.set_2nd_lo(float(v3),"usb")
 sis.set_2nd_lo(float(v4),"lsb")
+
+sis.set_1st_lo_onoff("on","lhcp")
+sis.set_1st_lo_onoff("on","rhcp")
+sis.set_2nd_lo_onoff("on","usb")
+sis.set_2nd_lo_onoff("on","lsb")
