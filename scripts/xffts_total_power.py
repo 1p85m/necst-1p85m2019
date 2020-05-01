4844da4e7ad5180454b087e06dfa16e7c5752dcd
kotakayoshi <tatuyuki.takashima@gmail.com>
2020-05-01 18:14:53 +0000

    bug fix

--- scripts/xffts_total_power.py
+++ scripts/xffts_total_power.py
@@ -12,7 +12,7 @@ class xffts_total_power(object):
     def __init__(self):
         self.board_num = rospy.get_param("~board_num")
         [rospy.Subscriber("/xffts_board0%d"%(i+1), Float64MultiArray, self.sum, callback_args = i) for i in range(int(self.board_num))]
-        self.pub = [rospy.Publisher("/xffts_board0%d/tp"%(i+1), Float64, queue_size=1) for i in range(int(self.board_num))]
+        self.pub = [rospy.Publisher("/xffts_board0%d/tp"%(i+1), Float64MultiArray, queue_size=1) for i in range(int(self.board_num))]
 
 
     def sum(self, q, arg):
