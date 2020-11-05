583180b2a853a827cb7826af62141c7c6cd3e8ed
takerumatsumoto <s_ta.matsumoto@p.s.osakafu-u.ac.jp>
2020-11-05 21:01:25 +0900

    bug fix

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-for i in {1..10}
+for i in {1..24}
 do
 echo start ps_obs
 ipython position_swich.py
 sleep 2
-if [ $(($i%2)) = 0 ]; then
+if [ $(($i%4)) = 0 ]; then
         echo skydip
         ipython skydip.py
         sleep 2
