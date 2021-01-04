0596a4508bbe29dbfa375f07e392e10a59e70f51
Ayu Konishi <63437641+ayu-nico@users.noreply.github.com>
2021-01-05 01:04:24 +0900

    Update obs_log_gspread.py

--- scripts/obs_log_gspread.py
+++ scripts/obs_log_gspread.py
@@ -12,6 +12,7 @@ from std_msgs.msg import Float64
 
 class obs_log_gspread(object):
 
+
     def __init__(self):
 
         json = "/home/exito/ros/src/necst-sisrx_b67/lib/observation-log-c7a8ff4b5240.json"
@@ -21,6 +22,7 @@ class obs_log_gspread(object):
         rospy.Subscriber("/obs_log/date_time",String,self.date)
         rospy.Subscriber("/obs_log/logger_path",String,self.logger_path)
 
+
     def connect_gspread(self,json,key):
         scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
         credentials = ServiceAccountCredentials.from_json_keyfile_name(json,scope)
@@ -28,6 +30,7 @@ class obs_log_gspread(object):
         SPREADSHEET_KEY = key
         worksheet = gc.open_by_key(SPREADSHEET_KEY).sheet1
         return worksheet
+
 
     def date(self,q):
         date = q.data
@@ -40,9 +43,11 @@ class obs_log_gspread(object):
         self.date_time = Y+'/'+m+'/'+d+' '+H+':'+M+':'+S
         return
 
+
     def logger_path(self,q):
         self.logger_path = q.data
         return
+
 
     def toAlpha(num):
         if num<=26:
@@ -54,7 +59,7 @@ class obs_log_gspread(object):
 
 
     def write_log(self):
-        df1 = pd.DataFrame(worksheet.get_all_values())
+        df1 = pd.DataFrame(self.ws.get_all_values())
         df1.columns = list(df.loc[0, :])
         df1.drop(0, inplace=True)
         df1.reset_index(inplace=True)
@@ -64,7 +69,7 @@ class obs_log_gspread(object):
         col_lastnum = len(df2.columns) # DataFrameの列数
         row_lastnum = len(df2.index)   # DataFrameの行数
 
-        cell_list = worksheet.range('A1:'+toAlpha(col_lastnum)+str(row_lastnum+1))
+        cell_list = self.ws.range('A1:'+toAlpha(col_lastnum)+str(row_lastnum+1))
         for cell in cell_list:
             if cell.row == 1:
                 val = df2.columns[cell.col-1]
@@ -72,10 +77,24 @@ class obs_log_gspread(object):
                 val = df2.iloc[cell.row-2][cell.col-1]
             cell.value = val
 
-        worksheet.update_cells(cell_list)
-    
+        self.ws.update_cells(cell_list)
+
+
+    def regist_gspread(self):
+        while not rospy.is_shutdown():
+            self.write_log()
+            time.sleep(3)
+            continue
+            
+            
+    def thread(self):
+        th = threading.Thread(target=self.regist_gspread)
+        th.setDaemon(True)
+        th.start()
+
 
 if __name__=='__main__':
     rospy.init_node(name)
     olg = obs_log_gspread()
+    olg.thread()
     rospy.spin()
